32cd2b19361e43aab8ff811474e55418e073d030
DiegoEstevee <155534408+DiegoEstevee@users.noreply.github.com>
2026-01-09 12:53:39 +0100

    Update video source and calibration file paths

--- src/ping_pong/02_track_ball.py
+++ src/ping_pong/02_track_ball.py
@@ -3,8 +3,8 @@ import numpy as np
 from collections import deque
 import os
 
-VIDEO_PATH = "data/videos/video_2.mp4"
-HOMO_PATH  = "data/calibration/table_homography2.npz"
+VIDEO_PATH = "data/videos/video_1.mp4"
+HOMO_PATH  = "data/calibration/table_homography1.npz"
 OUT_VIDEO_PATH = "results/02_track_ball_output.mp4"
 
 os.makedirs("results", exist_ok=True)
@@ -193,3 +193,4 @@ while True:
 cap.release()
 out_writer.release()
 cv2.destroyAllWindows()
+
